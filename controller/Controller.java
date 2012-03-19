817c7f734b8dea7f59a54e3cf768cc84253cb7c0
Wilhelm W Schive <wilumium@hotmail.com>
2012-03-19 14:30:07 +0100

    controller

---
+++ controller/Controller.java
@@ -0,0 +1,13 @@
+package controller;
+
+import model.User;
+
+public class Controller {
+
+	private User currentUser;
+	
+	public void setCurrentUser(User user){
+	this.currentUser = user;
+	//oppdatere user
+	}
+}
