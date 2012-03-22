d9580b9f351b2cf679ba1b26475986085aece879
Wilhelm W Schive <wilumium@hotmail.com>
2012-03-22 14:40:28 +0100

    appointment ting

--- Database/DBTest.java
+++ Database/DBTest.java
@@ -1,7 +1,11 @@
 package Database;
 
 import java.util.ArrayList;
+import java.util.Date;
 
+import model.Appointment;
+import model.Meeting;
+import model.MeetingRoom;
 import model.User;
 
 public class DBTest {
@@ -10,8 +14,16 @@ public class DBTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub		
-		DBUser.removeUser("kalle");
+		// TODO Auto-generated method stub	
+		ArrayList<User> users = new ArrayList<User>();
+		User test = new User("wtf");
+		users.add(test);
+		Appointment m퓍e = new Appointment(new Date(), "testing", 1, 2, test);
+
+		m퓍e.setId(DBAppointment.addAppointment(m퓍e));
+		
+		System.out.println(m퓍e.getId());
+
 	}
 
 }
